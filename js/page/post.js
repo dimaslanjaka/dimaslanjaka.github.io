56860e45e928307ea40dc78bd2afb20678093fa4
dimaslanjaka <dimaslanjaka@gmail.com>
2021-08-22 06:00:35 +0000

    Deploying to master from @ dimaslanjaka/dimaslanjaka.github.io@4787223b34137a592ae06fe515cb2713b09786f1 🚀

--- js/page/post.js
+++
@@ -1,1 +0,0 @@
-window.addEventListener("load",(function(){var e=document.querySelector("article.page__post");new Pack(e).base("js-ease-out-leave-active").base("js-ease-out-leave").transfrom("js-ease-out-enter-active").end((function(){["js-ease-out-enter","js-ease-out-enter-active","js-ease-out-leave","js-ease-out-leave-active"].forEach((function(a){e.classList.remove(a)}))})).toggle()}));

f0a2fc33f583091553bf3ea779eedf8189d6404f
dimaslanjaka <dimaslanjaka@gmail.com>
2022-02-12 05:15:26 +0000

    Deploying to master from @ dimaslanjaka/dimaslanjaka.github.io@fdeb9a57a003c893ec11d3db3d67d6e0629f2f2d 🚀

--- Chimeraland/Recipes/script.js
+++ Chimeraland/Recipes/script.js
@@ -1,13 +1,1 @@
-const td = document.querySelectorAll("article table#recipes td");
-Array.from(td).forEach((el) => {
-  let text = el.innerHTML;
-  text = text.replace(
-    "fullness",
-    '<img src="Recipes/fullness.jpg" class="img-inline-text" title="fullness" />'
-  );
-  text = text.replace(
-    "atk",
-    '<img src="Recipes/attack.png" class="img-inline-text atk" title="attack" />'
-  );
-  el.innerHTML = text;
-});
+const e=document.querySelectorAll("article table#recipes td");Array.from(e).forEach((e=>{let t=e.innerHTML;t=t.replace("fullness",'<img src="Recipes/fullness.jpg" class="img-inline-text" title="fullness" />'),t=t.replace("atk",'<img src="Recipes/attack.png" class="img-inline-text atk" title="attack" />'),e.innerHTML=t}));

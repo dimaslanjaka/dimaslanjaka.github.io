3e773a6a411ed755290aaa144c52ed490c8b50c3
dimaslanjaka <dimaslanjaka@gmail.com>
2022-03-31 23:18:56 +0700

    +scheduler

--- src/generator.ts
+++ src/generator.ts
@@ -1,6 +1,11 @@
 import taskCopy from './gulp/tasks/article-copy';
 import { existsSync, rmdirSync } from './node/filemanager';
+import scheduler from './node/scheduler';
 import { tmp } from './types/_config';
 if (existsSync(tmp())) rmdirSync(tmp());
 
-if (exports) exports.copy = taskCopy;
+// register scheduler
+new scheduler();
+
+// article copy
+if (typeof exports != 'undefined') exports.copy = taskCopy;

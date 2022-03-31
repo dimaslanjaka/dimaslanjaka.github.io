6559488274693746a1eb6598556eca5d7dd1a78a
dimaslanjaka <dimaslanjaka@gmail.com>
2022-04-01 01:33:07 +0700

    Update

--- src/gulp/tasks/article-generate.ts
+++ src/gulp/tasks/article-generate.ts
@@ -6,9 +6,13 @@ import config, { root } from '../../types/_config';
 import through from 'through2';
 import vinyl from 'vinyl';
 import 'js-prototypes';
+import Bluebird from 'bluebird';
 
 const source_dir = toUnix(resolve(join(root, config.source_dir)));
 const generated_dir = toUnix(resolve(join(root, config.public_dir)));
+export const theme_dir = toUnix(resolve(join(root, 'theme', config.theme)));
+console.log(theme_dir);
+
 //rmdirSync(generated_dir);
 export default function generate() {
   const src: string[] = [];
@@ -22,16 +26,14 @@ export default function generate() {
   const copyAssets = () => {
     include(['**/**']);
     // exclude markdown files
-    exclude(['_data/**', '_drafts/**', '**/**.md']);
-    return gulp
-      .src(src)
-      .pipe(
-        through.obj(function (file: vinyl, enc, cb) {
-          if (file.dirname.includes('/_posts')) file.dirname = file.dirname.replace('/_posts/', '/');
-          //const pathfile = toUnix(file.path);
-          //const contents = fn(String(file.contents), toUnix(file.path), file) || file.contents;
-          //file.contents = !Buffer.isBuffer(contents) ? Buffer.from(contents) : contents;
-          /*if (!file.basename.toString().match(/\.min\.[js,css,html]/gi)) {
+    exclude(['_data/**', '_drafts/**', '**/**.md', '_posts/**/**.md', '**/**.code-workspace']);
+    return gulp.src(src).pipe(
+      through.obj(function (file: vinyl, enc, cb) {
+        if (file.path.includes('/_posts')) file.dirname = file.dirname.replace('/_posts/', '/');
+        //const pathfile = toUnix(file.path);
+        //const contents = fn(String(file.contents), toUnix(file.path), file) || file.contents;
+        //file.contents = !Buffer.isBuffer(contents) ? Buffer.from(contents) : contents;
+        /*if (!file.basename.toString().match(/\.min\.[js,css,html]/gi)) {
           switch (file.extname) {
             case '.js':
               // minify js
@@ -42,29 +44,33 @@ export default function generate() {
               break;
           }
         }*/
-          cb(null, file);
-        })
-      )
-      .pipe(gulp.dest(generated_dir));
+        cb(null, file);
+      })
+    );
+    //.pipe(gulp.dest(generated_dir));
   };
 
   const renderArticle = () => {
     // only markdown files
     src.length = 0;
     include(['**.md', '_posts/**/**.md']);
-    exclude(['_data/**', '_drafts/**', '**/readme.md', '**/README.md']);
-    console.log(src);
-    return gulp.src(src).pipe(
+    exclude(['_data/**', '_drafts/**', '**/readme.md', '**/**.code-workspace']);
+    //console.log(src);
+    return gulp.src(src, { nocase: true }).pipe(
       through.obj((file: vinyl, encoding, cb) => {
         const filepath = toUnix(file.path);
         file.extname = '.html';
         //filepath.replace(cwd(), ''),
         if (file.dirname.includes('/_posts')) file.dirname = file.dirname.replace('/_posts/', '/');
-        if (file.dirname.match(/README/gi)) console.log(file.dirname);
+        if (file.dirname.match(/readme/gi)) console.log(file.dirname);
+
         cb(null, file);
       })
     );
   };
 
-  return copyAssets().once('end', () => renderArticle().pipe(gulp.dest(generated_dir)));
+  //return copyAssets().once('end', () => renderArticle().pipe(gulp.dest(generated_dir)));
+  Bluebird.resolve(copyAssets())
+    .then((copy) => [copy, renderArticle()])
+    .then((arr) => arr.map((s) => s.pipe(gulp.dest(generated_dir))));
 }

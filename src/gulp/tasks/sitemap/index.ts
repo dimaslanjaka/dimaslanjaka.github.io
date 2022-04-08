6b8343c5deafbb02d0dc3528e3fc94a66c443cf5
dimaslanjaka <dimaslanjaka@gmail.com>
2022-04-09 04:45:57 +0700

    +post-sitemap.xml generator

--- src/gulp/tasks/sitemap/index.ts
+++ src/gulp/tasks/sitemap/index.ts
@@ -6,14 +6,27 @@ import { join, readFileSync, write } from '../../../node/filemanager';
 import config, { post_generated_dir } from '../../../types/_config';
 import 'js-prototypes';
 import Bluebird from 'bluebird';
+import chalk from 'chalk';
 
+/// define global variable without refetch them
 const postCache = new CachePost();
+const logname = chalk.magentaBright('[sitemap-xml]');
+/**
+ * all mapped posts
+ */
+const allPosts = Bluebird.all(postCache.getAll()).map((i) => {
+  if (!i.metadata.type || !i.metadata.type.length)
+    if (i.fileTree) if (typeof i.fileTree.public == 'string') i.metadata.type = i.fileTree.public.includes('_posts') ? 'post' : 'page';
+  return i;
+});
 
 /**
  * copy asset sitemaps
  */
 function copy() {
-  return gulp.src('**/*.{xsd,xsl}', { cwd: join(__dirname, 'views') }).pipe(gulp.dest(post_generated_dir));
+  const srcdir = join(__dirname, 'views');
+  console.log(logname, 'copy', srcdir, '->', post_generated_dir);
+  return gulp.src('**/*.{xsd,xsl}', { cwd: srcdir }).pipe(gulp.dest(post_generated_dir));
 }
 
 function getLatestDateArray(arr: any[]) {
@@ -24,15 +37,40 @@ function getLatestDateArray(arr: any[]) {
   }
 }
 
+/**
+ * generate post sitemap
+ * @param done
+ */
+function generatePost(done?: TaskCallback) {
+  const sourceIndexXML = join(__dirname, 'views/post-sitemap.xml');
+  const readXML = readFileSync(sourceIndexXML, 'utf-8');
+  allPosts
+    .map((post) => {
+      const lastmod = moment(post.metadata.updated).format('YYYY-MM-DDTHH:mm:ssZ');
+      return `<url><loc>${post.metadata.url}</loc><lastmod>${lastmod}</lastmod></url>`;
+    })
+    .then((xmls) => {
+      const buildXML = readXML.replace(/<url>+[\s\S\n]*<\/url>/gm, xmls.join('\n'));
+      write(join(post_generated_dir, 'post-sitemap.xml'), buildXML).then((f) => {
+        console.log(f);
+        if (typeof done == 'function') done();
+      });
+    });
+}
+
+/**
+ * generated sitemap index
+ * @see {@link https://yoast.com/sitemap_index.xml}
+ * @param done
+ */
 async function generateIndex(done?: TaskCallback) {
   const sourceIndexXML = join(__dirname, 'views/sitemap.xml');
   const readXML = readFileSync(sourceIndexXML, 'utf-8');
 
-  const all = Bluebird.all(postCache.getAll());
   /**
    * get latest date of tags
    */
-  const latestTag = await all
+  const latestTag = await allPosts
     .map((item) => {
       if (item.metadata.tags && item.metadata.tags.length) {
         if (item.metadata.updated) return moment(item.metadata.updated);
@@ -46,7 +84,7 @@ async function generateIndex(done?: TaskCallback) {
   /**
    * get latest date of categories
    */
-  const latestCat = await all
+  const latestCat = await allPosts
     .map((item) => {
       if (item.metadata.category && item.metadata.category.length) {
         if (item.metadata.updated) return moment(item.metadata.updated);
@@ -58,7 +96,7 @@ async function generateIndex(done?: TaskCallback) {
       return `<sitemap><loc>${config.url.replace(/\/+$/, '')}/category-sitemap.xml</loc><lastmod>${date}</lastmod></sitemap>`;
     });
 
-  const latestPost = await all
+  const latestPost = await allPosts
     .map((item) => {
       if (item.metadata.type && item.metadata.type == 'post') {
         if (item.metadata.updated) return moment(item.metadata.updated);
@@ -70,7 +108,7 @@ async function generateIndex(done?: TaskCallback) {
       return `<sitemap><loc>${config.url.replace(/\/+$/, '')}/post-sitemap.xml</loc><lastmod>${date}</lastmod></sitemap>`;
     });
 
-  const latestPage = await all
+  const latestPage = await allPosts
     .map((item) => {
       if (item.metadata.type && item.metadata.type == 'page') {
         if (item.metadata.updated) return moment(item.metadata.updated);
@@ -92,7 +130,7 @@ async function generateIndex(done?: TaskCallback) {
   });
 }
 
-gulp.task('generate:sitemap-xml', gulp.series(copy, generateIndex));
+gulp.task('generate:sitemap-xml', gulp.series(copy, generateIndex, generatePost));
 
 interface sitemapItem {
   loc: string;

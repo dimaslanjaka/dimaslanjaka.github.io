4fd36e18a09bd94f356424bb383fb92e909f3427
dimaslanjaka <dimaslanjaka@gmail.com>
2022-04-11 19:12:28 +0700

    delete unused configuration

--- src/gulp/tasks/generate-feed.ts
+++ src/gulp/tasks/generate-feed.ts
@@ -1,6 +1,7 @@
 import { Feed } from 'feed';
+import moment from 'moment';
 import { helpers } from '../../ejs';
-import { author_link, author_name } from '../../ejs/helper/author';
+import { author_email, author_link, author_name } from '../../ejs/helper/author';
 import config from '../../types/_config';
 
 const homepage = new URL(config.url);
@@ -13,17 +14,17 @@ const feed = new Feed({
   link: config.url,
   language: 'en', // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
   image: 'http://example.com/image.png',
-  favicon: 'http://example.com/favicon.ico',
-  copyright: 'All rights reserved 2013, John Doe',
+  favicon: urlfor('/favicon.ico'),
+  copyright: 'All rights reserved ' + moment().format('YYYY') + ', ' + author_name(config),
   updated: new Date(2013, 6, 14), // optional, default = today
-  generator: 'awesome', // optional, default = 'Feed for Node.js'
+  generator: 'static blog generator', // optional, default = 'Feed for Node.js'
   feedLinks: {
     json: urlfor('/feed.json'),
     atom: urlfor('/atom.xml'),
   },
   author: {
     name: author_name(config),
-    email: 'johndoe@example.com',
+    email: author_email(config),
     link: author_link(config),
   },
 });

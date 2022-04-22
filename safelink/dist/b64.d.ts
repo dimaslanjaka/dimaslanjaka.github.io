3a726a9320873759744b09ec9c0ab12a8dcfc741
dimaslanjaka <dimaslanjaka@gmail.com>
2022-04-22 15:36:26 +0000

    Deploying to master from @ dimaslanjaka/dimaslanjaka.github.io@70143183210aebafef81b7d80ff796b631bce6c1 🚀

--- safelink/dist/b64.d.ts
+++
@@ -1,6 +0,0 @@
-import { Nullable } from './resolveQueryUrl';
-export declare const b64: {
-    encode: (str: string) => Nullable<string>;
-    decode: (encoded: string) => Nullable<string>;
-};
-export default b64;
--- safelink/dist/toURL.d.ts
+++
@@ -1,7 +0,0 @@
-import { Nullable } from './resolveQueryUrl';
-/**
- * transform url string to {@link Nullable}<{@link URL}>
- * @param url
- * @returns
- */
-export default function toURL(url: string): Nullable<URL>;

61c90e6a199ca046aeab646ee698ddffe66bd6a4
Dimas Lanjaka <dimaslanjaka@gmail.com>
2022-04-23 19:13:36 +0700

    add .nojekyll automatically

--- src/gulp/tasks/generate.ts
+++ src/gulp/tasks/generate.ts
@@ -5,5 +5,13 @@ import './generate-feed';
 import './generate-posts';
 import './minify';
 import gulp from 'gulp';
+import { join, resolve, toUnix } from 'upath';
+import config, { root } from '../../types/_config';
+import { existsSync, write } from '../../node/filemanager';
+
+const generated_dir = toUnix(resolve(join(root, config.public_dir)));
+// .nojekyll
+const nojekyll = join(generated_dir, '.nojeklyy');
+if (!existsSync(nojekyll)) write(nojekyll, '');
 
 gulp.task('generate', gulp.series('generate:assets', 'generate:template', 'generate:posts', 'generate:archive', 'generate:sitemap', 'generate:after'));

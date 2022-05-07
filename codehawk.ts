3d3cdaf09cbacdc4efde4f13f8c5c853025083e5
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-07 21:51:56 +0700

    [codehawk] use cli instead

--- codehawk.ts
+++
@@ -1,7 +0,0 @@
-import { analyzeProject } from 'codehawk-cli';
-const output = analyzeProject(__dirname); // returns a Results object
-
-// Get summary maintainability scores
-const { average, median, worst } = output.summary;
-
-console.log(average, median, worst);

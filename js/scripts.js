187c612cdd10b9b100fb2fdb390e0fa029fe10f9
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-11 02:46:58 +0000

    Deploying to master from @ dimaslanjaka/dimaslanjaka.github.io@cc07233cc22a0b8c6fa322d998535ef07f2da765 🚀

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,44 @@
  */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+document.onreadystatechange = function () {
+  if (document.readyState == 'complete') {
+    initClipboard();
+  }
+};
+
+/**
+ * * source {@link https://github.com/zenorocha/clipboard.js/blob/master/dist/clipboard.min.js}
+ * * development {@link https://raw.githack.com/zenorocha/clipboard.js/master/dist/clipboard.min.js}
+ * * production {@link https://rawcdn.githack.com/zenorocha/clipboard.js/2b2f9eef6fd1cf951612740e16e422db2848c00a/dist/clipboard.min.js}
+ */
+function initClipboard() {
+  var clipboardLoaded = false;
+  window.addEventListener(
+    'scroll',
+    function () {
+      var notop = 0 != document.documentElement.scrollTop;
+      var notopbody = 0 != document.body.scrollTop;
+      var noload = false === clipboardLoaded;
+      if (noload && (notop || notopbody)) {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src =
+          'https://rawcdn.githack.com/zenorocha/clipboard.js/2b2f9eef6fd1cf951612740e16e422db2848c00a/dist/clipboard.min.js';
+        script.setAttribute('crossorigin', 'anonymous');
+        script.onload = function () {
+          const clip = new ClipboardJS('[copy-to-clipboard]');
+          clip.on('success', function (_) {
+            console.log('codes copied');
+          });
+        };
+        var target = document.getElementsByTagName('script')[0];
+        target.parentNode.insertBefore(script, target);
+        clipboardLoaded = true;
+      }
+    },
+    true
+  );
+}

70c601b6aad4fa9550e87442169a4edad3ede3f7
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-19 11:37:14 +0700

    update deps

--- themes/bootstrap/source/js/scripts.js
+++ themes/bootstrap/source/js/scripts.js
@@ -3,7 +3,6 @@
  * Copyright 2013-2022 Start Bootstrap
  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-home/blob/master/LICENSE)
  */
-// This file is intentionally blank
 // Use this file to add JavaScript to your project
 
 document.onreadystatechange = function () {

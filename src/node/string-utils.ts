4d7ef228846f8486b7882ca317cab2df3d37cee8
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-21 13:17:21 +0700

    integrate ES2021 prototype

--- src/node/string-utils.ts
+++ src/node/string-utils.ts
@@ -68,3 +68,25 @@ export function replaceArr(str: string, array: string[], replacement: string) {
   });
   return ori;
 }
+
+interface String {
+  /**
+   * Replace all instances of a substring in a string, using a regular expression or search string.
+   * @param searchValue A string to search for.
+   * @param replaceValue A string containing the text to replace for every successful match of searchValue in this string.
+   */
+  replaceAll(searchValue: string | RegExp, replaceValue: string): string;
+
+  /**
+   * Replace all instances of a substring in a string, using a regular expression or search string.
+   * @param searchValue A string to search for.
+   * @param replacer A function that returns the replacement text.
+   */
+  replaceAll(searchValue: string | RegExp, replacer: (substring: string, ...args: any[]) => string): string;
+}
+if (typeof ''.replaceAll != 'function') {
+  String.prototype.replaceAll = function (search, replacement) {
+    const find = typeof search == 'string' ? new RegExp(search, 'g') : search;
+    return this.replace(find, replacement);
+  };
+}

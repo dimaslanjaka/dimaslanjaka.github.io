efd09c3b0281876965813a62370de18ea041d213
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-21 13:00:17 +0700

    clean js-prototypes func

--- src/ejs/helper/keywords.ts
+++ src/ejs/helper/keywords.ts
@@ -1,20 +1,18 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
 import { postMap } from '../../markdown/transformPosts/parsePost';
+import { uniqueStringArray } from '../../node/array-utils';
 
 export function keywords(page: postMap['metadata']) {
   let kw = [];
   if (Array.isArray(page.tags) && page.tags.length > 0) page.tags.forEach((i) => kw.push(i));
   if (Array.isArray(page.category) && page.category.length > 0) page.category.forEach((i) => kw.push(i));
   if (page.title) {
-    page.title
-      .replace(/[^\w\s]/gi, '')
-      .split(/\s+/)
-      .uniqueStringArray()
+    uniqueStringArray(page.title.replace(/[^\w\s]/gi, '').split(/\s+/))
       .map((s) => s.trim())
       .forEach((i) => kw.push(i));
   }
-  kw = kw.uniqueStringArray();
+  kw = uniqueStringArray(kw);
   return kw.join(',');
 }
 

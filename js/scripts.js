30181fdec38ecd828915c8270a6969e163275f62
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-21 02:15:33 +0000

    Deploying to master from @ dimaslanjaka/dimaslanjaka.github.io@2005c7b6d5773d63c023883085dbe4bb4aec35af 🚀

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
  * Copyright 2013-2022 Start Bootstrap
  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-home/blob/master/LICENSE)
  */
+// This file is intentionally blank
 // Use this file to add JavaScript to your project
 
 document.onreadystatechange = function () {

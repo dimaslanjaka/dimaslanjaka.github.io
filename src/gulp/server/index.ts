cbbe5885662cf1e965fa01eee2e4c4088b98a106
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-21 14:16:33 +0700

    +port killer

--- src/gulp/server/index.ts
+++ src/gulp/server/index.ts
@@ -1,22 +1,41 @@
 import Bluebird from 'bluebird';
 import { create as createServer } from 'browser-sync';
 import { ChildProcess } from 'child_process';
+import { kill } from 'cross-port-killer';
 import spawn from 'cross-spawn';
 import gulp from 'gulp';
 import memoizee from 'memoizee';
+import net from 'net';
 import { join, toUnix } from 'upath';
 import config from '../../types/_config';
 import { ServerMiddleWare } from './middleware';
 
 const cwd = memoizee(() => toUnix(process.cwd()));
 const browserSync = createServer();
-export function localServer() {
-  const bsi = browserSync.init({
-    server: './' + config.public_dir,
-    port: config.server.port,
-    open: false,
-    middleware: ServerMiddleWare
+const portInUse = function (port: number, callback: (arg0: boolean) => any) {
+  const server = net.createServer(function (socket) {
+    socket.write('Echo server\r\n');
+    socket.pipe(socket);
   });
+
+  server.on('error', function (_e) {
+    callback(true);
+  });
+  server.on('listening', function (_e: any) {
+    server.close();
+    callback(false);
+  });
+
+  server.listen(port, '127.0.0.1');
+};
+const options = {
+  server: './' + config.public_dir,
+  port: config.server.port,
+  open: false,
+  middleware: ServerMiddleWare
+};
+function startServer() {
+  const bsi = browserSync.init(options);
 
   // handling spawner to reduce memory usages
   const childs: { [key: string]: ChildProcess[] } = {
@@ -49,5 +68,16 @@ export function localServer() {
   });
   // watch public dir/.guid to reload browsersync
   //gulp.watch(join(config.public_dir, '.guid')).on('change', browserSync.reload);
-  return Promise.resolve(bsi);
+
+  return bsi;
+}
+
+export async function localServer() {
+  return portInUse(options.port, async (inuse) => {
+    if (inuse) {
+      const pids = await kill(options.port);
+      console.log(pids, 'killed');
+    }
+    return startServer();
+  });
 }

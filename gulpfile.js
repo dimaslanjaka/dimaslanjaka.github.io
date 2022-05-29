b57708dd895f89fc724ae48407b57cc9b46d9ad9
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-29 18:56:50 +0700

    [fixme] update

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,8 @@
 const gulp = require('gulp');
 const parser = require('hexo-post-parser');
 const path = require('path');
+const { copyPosts } = require('static-blog-generator/dist/src/gulp/tasks/copy');
 
-gulp.task('copy', async function () {
-  await parser.parsePost(
-    path.join(__dirname, 'src-posts/Tests/shortcodes.md'),
-    {
-      cache: false,
-      fix: true
-    }
-  );
+gulp.task('hexo:copy', async function () {
+  copyPosts(null, 'Tests/shortcodes');
 });

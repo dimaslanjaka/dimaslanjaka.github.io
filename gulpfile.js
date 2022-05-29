222dec36e83e2d702c94f1e9a36f140cf2fb737e
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-29 19:10:12 +0700

    direct copy post

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 const gulp = require('gulp');
-const parser = require('hexo-post-parser');
-const path = require('path');
 const { copyPosts } = require('static-blog-generator/dist/src/gulp/tasks/copy');
 
 gulp.task('hexo:copy', async function () {

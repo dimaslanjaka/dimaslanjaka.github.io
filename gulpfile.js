2378d74a5d5a3e89400596c0bb8dee0a179b6be3
dimaslanjaka <dimaslanjaka@gmail.com>
2022-05-31 19:15:36 +0700

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ const gulp = require('gulp');
 const { copyPosts, clean_posts, copyAssets } = require('static-blog-generator');
 
 gulp.task('hexo:copy', async function (done) {
-  copyAssets();
-  copyPosts(done, 'Tests/shortcodes');
+  const assets = copyAssets();
+  if (!('then' in assets)) {
+    copyPosts(done, 'Tests/shortcodes');
+  }
 });
 
 gulp.task('hexo:clean', async function (done) {
-  clean_posts(done)
+  clean_posts(done);
 });

f9f648fde0e26e37422a8b221ddbff25786baa36
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-01 16:58:01 +0700

    update post installer

--- postinstall.js
+++ postinstall.js
@@ -1,18 +1,98 @@
-const fs = require('fs');
-const path = require('path');
 const { spawn } = require('child_process');
+const path = require('path');
 
 const sbgPath = path.join(__dirname, 'packages/static-blog-generator');
-spawn('yarn', ['install', '--check-files'], { cwd: sbgPath });
-spawn('yarn', ['install', '--check-files'], {
-  cwd: path.join(sbgPath, 'packages/safelink')
+
+summon(
+  'git',
+  ['submodule', 'sync', '--recursive'],
+  spawnOpt({ cwd: sbgPath })
+).then(() => {
+  summon(
+    'git',
+    ['submodule', 'sync', '--recursive'],
+    spawnOpt({
+      cwd: path.join(sbgPath, 'packages/hexo-post-parser')
+    })
+  ).then(() => {
+    // begin installing
+    summon(
+      'yarn',
+      ['install', '--check-files'],
+      spawnOpt({
+        cwd: path.join(
+          sbgPath,
+          'packages/hexo-post-parser/packages/persistent-cache'
+        )
+      })
+    ).then(() => {
+      summon(
+        'yarn',
+        ['install', '--check-files'],
+        spawnOpt({
+          cwd: path.join(sbgPath, 'packages/google-news-sitemap')
+        })
+      ).then(() => {
+        summon(
+          'yarn',
+          ['install', '--check-files'],
+          spawnOpt({
+            cwd: path.join(sbgPath, 'packages/hexo-post-parser')
+          })
+        ).then(() => {
+          summon(
+            'yarn',
+            ['install', '--check-files'],
+            spawnOpt({
+              cwd: path.join(sbgPath, 'packages/safelink')
+            })
+          ).then(() => {
+            summon(
+              'yarn',
+              ['install', '--check-files'],
+              spawnOpt({ cwd: sbgPath })
+            );
+          });
+        });
+      });
+    });
+  });
 });
-spawn('yarn', ['install', '--check-files'], {
-  cwd: path.join(sbgPath, 'packages/hexo-post-parser')
-});
-spawn('yarn', ['install', '--check-files'], {
-  cwd: path.join(sbgPath, 'packages/google-news-sitemap')
-});
-spawn('yarn', ['install', '--check-files'], {
-  cwd: path.join(sbgPath, 'packages/hexo-post-parser/packages/persistent-cache')
-});
+
+function summon(cmd, args = [], opt = {}) {
+  // *** Return the promise
+  return new Promise(function (resolve, reject) {
+    if (typeof cmd !== 'string' || cmd.length === 0) return reject('cmd empty');
+    const process = spawn(cmd, args, opt);
+    process.on('close', function (code) {
+      // Should probably be 'exit', not 'close'
+      // *** Process completed
+      resolve(code);
+    });
+    process.on('error', function (err) {
+      // *** Process creation failed
+      reject(err);
+    });
+  });
+}
+
+/**
+ * Overidden Options
+ * @param {import('child_process').SpawnOptions} opt
+ * @returns
+ */
+function spawnOpt(opt = {}) {
+  return Object.assign({ stdio: 'inherit' }, opt);
+}
+
+/*
+cdir="$PWD"
+
+git submodule update -i -r
+echo "update submodule static-blog-generator"
+cd $cdir/packages/static-blog-generator
+git submodule update -i -r
+echo "update submodule hexo-post-parser"
+cd $cdir/packages/static-blog-generator/packages/hexo-post-parser
+git submodule update -i -r
+*/

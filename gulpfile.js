cc20833abcd269d5f9f86a4551933a134a65acef
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-04 03:13:15 +0700

    update tasks

--- gulpfile.js
+++ gulpfile.js
@@ -13,50 +13,19 @@ const {
   getConfig
 } = require('static-blog-generator');
 const { join } = require('path');
-initGulp({
-  copyPosts,
-  clean_tmp,
-  clean_db,
-  clean_posts,
-  copyAssets,
-  parsePost,
-  getConfig
-});
 
 async function copy() {
   const config = getConfig();
-  const source = join(__dirname, '');
-  console.log(config);
+  const source = join(__dirname, config.source_dir);
+  copyAssets();
+  copyPosts(null, null, { shortcodes: { codeblock: false } });
+}
+
+async function task_clean() {
+  clean_posts();
+  clean_tmp();
+  clean_db();
 }
 
 gulp.task('copy', copy);
-
-/**
- * initialize gulp
- * @param {import('static-blog-generator')} ins
- */
-function initGulp(ins = {}) {
-  gulp.task('hexo:clean', async () => task_clean(ins));
-}
-
-/**
- * copy task
- * @param {import('static-blog-generator')} ins
- */
-function task_copy(ins = {}) {
-  const assets = ins.copyAssets();
-  if (!('then' in assets)) {
-    // copyPosts(null, 'Tests/shortcodes');
-    ins.copyPosts(null, null);
-  }
-}
-
-/**
- * clean task
- * @param {import('static-blog-generator')} ins
- */
-function task_clean(ins = {}) {
-  ins.clean_posts();
-  ins.clean_tmp();
-  ins.clean_db();
-}
+gulp.task('clean', task_clean);

443a7601e8332d440ed69eb5ee1a598d50eaa776
dimaslanjaka <dimaslanjaka@gmail.com>
2022-06-06 03:05:30 +0700

    update
    Signed-off-by: dimaslanjaka <dimaslanjaka@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,14 @@ var Hexo = require('hexo');
 var gulp = require('gulp');
 var hexo = new Hexo(process.cwd(), {});
 
-
-
 gulp.task('default', async () => {
   hexo.init().then(function () {
     hexo.load().then(function (err, val) {
-      hexo.locals.invalidate();
-      console.log(hexo.locals.get('posts'));
+      //hexo.locals.invalidate();
+      const posts = hexo.locals.get('posts');
+      posts.forEach((post) => {
+        console.log(post.title);
+      });
     });
   });
 });

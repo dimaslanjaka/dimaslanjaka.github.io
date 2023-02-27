61be690bdd9e705e43c983dd03a1e9f6a5d50ea0
dimaslanjaka <dimaslanjaka@gmail.com>
2023-02-28 02:27:32 +0700

    add package installer

--- github-actions/index.js
+++ github-actions/index.js
@@ -1,10 +1,17 @@
 const fs = require("fs");
+const { spawn } = require("git-command-helper");
 const args = require("minimist")(process.argv.slice(2));
 const root = process.cwd();
 const jsdom = require("jsdom");
 const path = require("path");
 
 validate(path.join(root, "index.html"), "homepage");
+
+spawn("npm", ["install", "--omit=dev", "--production"], {
+	cwd: path.join(root, "page"),
+}).finally(() => {
+	spawn("npm", ["install", "--omit=dev", "--production"], { cwd: root });
+});
 
 /**
  * start validation

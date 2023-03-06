fc45b54a58ca1dc0a0353e55d5adf5b6d13c9036
dimaslanjaka <dimaslanjaka@gmail.com>
2023-03-06 16:13:50 +0700

    chore: released from https://github.com/dimaslanjaka/static-blog-generator-hexo/commit/4ee95c4562065463870c8b654330d899d8ef2f92
    commit hash: 4ee95c
    
    commit url: https://github.com/dimaslanjaka/static-blog-generator-hexo/commit/4ee95c4562065463870c8b654330d899d8ef2f92
    
    runner: https://github.com/dimaslanjaka/static-blog-generator-hexo/commit/4ee95c4562065463870c8b654330d899d8ef2f92/checks/4341652298

--- hexo-seo-js/concat-d680e8a854a7cbad6d490c445cba2eba.js
+++ hexo-seo-js/concat-d680e8a854a7cbad6d490c445cba2eba.js
@@ -1,460 +1,81 @@
 /*--- inner-0 --*/
 
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q || []).push(arguments)},i[r].l=1 * new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-106238155-1', 'auto');
-ga('send', 'pageview');
-
-
-
-/*--- /js/common.js --*/
-
-
-window.$claudia = {
-    throttle: function (func, time) {
-        var wait = false
-        return function () {
-            if (wait) return
-            wait = true
-
-            setTimeout(function () {
-                func()
-                wait = false
-            }, time || 100)
+    const searchURL = "https://www.webmanajemen.com/search.xml";
+    fetch(searchURL).then(response => response.text()).then(response => {
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(response, "text/xml");
+      const root = xmlDoc.getElementsByTagName("search")[0]
+      const baseURL = new URL('https://www.webmanajemen.com')
+      const results = Array.from(root.children).map(el => {
+        baseURL.pathname = el.querySelector('url').innerHTML.trim().replace(/^\/+/, '/')
+        const title = el.querySelector('title').innerHTML.trim()
+        return {
+          title,
+          content: el.querySelector('content').innerHTML.trim(),
+          url: baseURL.toString(),
+          card: document.querySelector(
+            'div.card[title="' + title + '"]'
+          )
         }
-    },
-    fadeInImage: function(imgs, imageLoadedCallback) {
-        var images = imgs || document.querySelectorAll('.js-img-fadeIn')
+      })
 
-        function loaded(event) {
-            var image = event.currentTarget
+      const querySearch = getParameterByName('q')
+      if (querySearch) {
+        document.getElementById('title').innerHTML = querySearch + ' Search Result'
+      } else {
+        document.getElementById('title').innerHTML = 'WMI Site Search'
+      }
 
-            image.ontransitionend = function () {
-                image.ontransitionend = null
-                image.style.transition = null
+      results.forEach(item => {
+        const card = item.card
+        if (card) {
+          // supress image error
+          const checkImageBroken = function () {
+            const img = card.querySelector('img[source]')
+            const source = img.getAttribute('source')
+            const imgElement = new Image();
+            imgElement.addEventListener('load', () => (img.src = source));
+            //imgElement.addEventListener('error', () => resolve(false));
+            imgElement.src = source
+          }
+
+
+          if (querySearch) {
+            const matchTitle = new RegExp(querySearch, 'igm').test(item.title)
+            const matchContent = new RegExp(querySearch, 'igm').test(item.content)
+            if (matchTitle || matchContent) {
+              // matched
+              checkImageBroken()
+            } else {
+              card.remove()
             }
-            image.style.transition = 'opacity 320ms'
-            image.style.opacity = 1
-
-            if (image.parentElement && image.parentElement.classList.contains('skeleton')) {
-                image.parentElement.classList.remove('skeleton')
-            }
-            imageLoadedCallback && imageLoadedCallback(image)
+          }
         }
+      })
+    });
 
-        images.forEach(function (img) {
-            if (img.complete) {
-                return loaded({ currentTarget: img })
-            }
+    document.getElementById('form-search').addEventListener('submit', function (_e) {
+      _e.preventDefault()
+      location.href = '/search/?q=' + _e.target.elements.q.value
+      return false
+    })
 
-            img.addEventListener('load', loaded)
-        })
-    },
-    blurBackdropImg: function(image) {
-        if (!image.dataset.backdrop) return
-
-        var parent = image.parentElement //TODO: Not finish yes, must be a pure function
-        var parentWidth = Math.round(parent.getBoundingClientRect().width)
-        var childImgWidth = Math.round(image.getBoundingClientRect().width)
-
-        var isCovered = parentWidth === childImgWidth
-        var blurImg = parent.previousElementSibling //TODO: Not finish yes, must be a pure function
-
-        isCovered ? blurImg.classList.add('is-hidden') : blurImg.classList.remove('is-hidden')
-    },
-    getSystemTheme(callback) {
-        var media = window.matchMedia('(prefers-color-scheme: dark)')
-        media.addEventListener('change', function (e){
-            callback && callback(e.matches ? "dark" : "light")
-        })
-
-        callback && callback(media.matches ? 'dark' : 'light')
+    function cardClickHandler(el) {
+      const perm = el.getAttribute('permalink')
+      //window.location.href = perm
+      window.open(perm, 'wmi').focus()
     }
-}
 
-
-
-
-/*--- /r-ads.js --*/
-
-
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-global-assign */
-
-/// <reference path="./cookie.js" />
-
-/**
- * ADSENSE FULLY AUTOMATIC
- */
-
-document.addEventListener('DOMContentLoaded', function () {
-	if (!islocalhost()) {
-		window.addEventListener('scroll', triggerAdsense);
-	} else {
-		triggerAdsense();
-	}
-});
-
-/**
- * Prevent Duplicate
- */
-let called = false;
-
-/**
- * Trigger adsense
- * @param {Event} _e
- * @returns
- */
-function triggerAdsense(_e) {
-	if (called) return;
-	called = true;
-
-	/**
-	 * debug on localhost
-	 */
-	const log = islocalhost()
-		? console.log
-		: function (..._args) {
-				//
-		  };
-
-	const existingIns = Array.from(document.querySelectorAll('ins[class*=adsbygoogle]'));
-	log('existing ins', existingIns.length);
-
-	for (let i = 0; i < existingIns.length; i++) {
-		const ins = existingIns[i];
-
-		if (islocalhost()) {
-			log('apply test ad to existing ins', i + 1);
-			ins.setAttribute('data-adtest', 'on');
-		}
-	}
-
-	/**
-	 * do not show ads to these page title
-	 */
-	const banned = [/lagu|jackpot|montok|hack|crack|nulled/gi];
-	if (banned.map(regex => regex.test(document.title)).some(result => result == true)) {
-		// skip showing ads from banned page
-		return;
-	}
-
-	const allAds = [
-		{
-			name: 'kiki',
-			pub: '2188063137129806',
-			ads: [
-				{
-					style: 'display: block; text-align: center',
-					'data-ad-layout': 'in-article',
-					'data-ad-format': 'fluid',
-					'data-ad-slot': '5634823028',
-				},
-				{
-					style: 'display: block; text-align: center',
-					'data-ad-layout': 'in-article',
-					'data-ad-format': 'fluid',
-					'data-ad-slot': '8481296455',
-				},
-				{
-					style: 'display:block',
-					'data-ad-slot': '2667720583',
-					'data-ad-format': 'auto',
-					'data-full-width-responsive': 'true',
-				},
-				{
-					style: 'display:block',
-					'data-ad-format': 'fluid',
-					'data-ad-layout-key': '-gw-3+1f-3d+2z',
-					'data-ad-slot': '6979059162',
-				},
-			],
-		},
-	].sort(function () {
-		// shuffle
-		return 0.5 - Math.random();
-	});
-
-	// select ads
-	// cookie key
-	const ck = 'currentAds';
-	// select previous ads id from cookie
-	const ca = getCookie(ck);
-	/**
-	 * @type {typeof allAds[number]}
-	 */
-	let currentSlot = allAds.find(item => item.pub === ca);
-
-	if (ca.length > 0 && typeof currentSlot === 'object') {
-		log('cached pub', ca);
-	} else {
-		currentSlot = allAds[0];
-
-		if (location.pathname != '/') {
-			log('caching pub', currentSlot.pub);
-			setCookie(
-				ck,
-				currentSlot.pub,
-				1,
-				location.pathname,
-				location.hostname,
-				location.protocol.includes('https') && location.host === 'www.webmanajemen.com',
-			);
-		}
-	}
-
-	log('total ads banner', currentSlot.ads.length);
-
-	// find element *[adsense="fill"]
-	const fixedPlacement = Array.from(document.querySelectorAll('[adsense="fill"]'));
-	if (fixedPlacement.length > 0) {
-		for (let i = 0; i < fixedPlacement.length; i++) {
-			const place = fixedPlacement[i];
-			const attr = currentSlot.ads.shift();
-			if (attr) {
-				attr['data-ad-client'] = 'ca-pub-' + currentSlot.pub;
-				const ins = createIns(attr);
-				log('insert ads to adsense="fill"', i + 1);
-				replaceWith(ins, place);
-			}
-		}
-	}
-
-	// find content/article wrapper
-	let findPlaces = Array.from(document.querySelectorAll('article'));
-	if (findPlaces.length === 0) {
-		// theme-next main content
-		findPlaces = Array.from(document.querySelectorAll('.page.main-inner'));
-	}
-	if (findPlaces.length === 0) {
-		findPlaces = Array.from(document.querySelectorAll('#main-content'));
-	}
-	if (findPlaces.length === 0) {
-		// bootstrap wrapper
-		findPlaces = Array.from(document.querySelectorAll('#bootstrap-wrapper'));
-	}
-	if (findPlaces.length === 0) {
-		// typedoc documentation page
-		findPlaces = Array.from(document.querySelectorAll('[class="col-8 col-content"]'));
-	}
-	// fallback search at body
-	if (findPlaces.length === 0) {
-		findPlaces = Array.from(document.querySelectorAll('body'));
-	}
-	
-	// select random place
-	let adsPlaces = findPlaces
-		.map(getAllPlaces)
-		.flat(1)
-		.sort(function () {
-			return 0.5 - Math.random();
-		})
-		.filter(el => el !== null)
-		.sort(function () {
-			return 0.5 - Math.random();
-		});
-
-	log('total targeted ads places', adsPlaces.length);
-
-	if (adsPlaces.length > 0 && currentSlot.ads.length > 0) {
-		for (let i = 0; i < currentSlot.ads.length; i++) {
-			const attr = currentSlot.ads[i];
-			if (attr) {
-				attr['data-ad-client'] = 'ca-pub-' + currentSlot.pub;
-				const ins = createIns(attr);
-				let nextOf = adsPlaces.shift(); // get first element and remove it from list
-				while (!nextOf) {
-					// find next when nextOf = null
-					if (adsPlaces.length > 0) {
-						// select next place
-						nextOf = adsPlaces.shift();
-					} else {
-						// if ads places empty, put to any div
-						nextOf = document.querySelector('div');
-					}
-				}
-
-				log('add banner', i + 1);
-				insertAfter(ins, nextOf);
-			}
-		}
-	}
-
-	// create pagead
-	const script = document.createElement('script');
-	script.src = `//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${currentSlot.pub}`;
-	script.async = true;
-	script.setAttribute('crossorigin', 'anonymous');
-	document.head.appendChild(script);
-
-	const allIns = Array.from(document.querySelectorAll('ins'));
-	log('total ins', allIns.length);
-
-	for (let i = 0; i < allIns.length; i++) {
-		log('apply banner', i + 1);
-		const ins = allIns[i];
-		if (!ins) continue;
-		if (!ins.getAttribute('data-ad-client')) {
-			console.log(ins);
-			continue;
-		}
-		const adclient = ins.getAttribute('data-ad-client').replace('ca-pub-', '');
-		const anonclient = adclient.slice(0, 3) + 'xxx' + adclient.slice(adclient.length - 3);
-		const adsid = ins.getAttribute('data-ad-slot');
-		const anonid = adsid.slice(0, 3) + 'xxx' + adsid.slice(adsid.length - 3);
-		const bg = `https://via.placeholder.com/200x50/FFFFFF/000000/?text=${anonclient}-${anonid}`;
-		ins.style.backgroundImage = `url('${bg}')`;
-		ins.style.backgroundRepeat = 'no-repeat';
-		// log('parent width banner', i + 0, ins.parentElement.offsetWidth);
-
-		if (ins.parentElement.offsetWidth === 0) {
-			// remove banner when parent width is 0 or display: none
-			log('remove banner', i + 1);
-			ins.remove();
-		} else if (ins.innerHTML.trim().length === 0) {
-			(adsbygoogle = window.adsbygoogle || []).push({});
-		}
-	}
-
-	/** FUNC START */
-
-	/**
-	 * create ins
-	 * @param {Record<string,any>} attributes
-	 * @returns
-	 */
-	function createIns(attributes) {
-		if (!attributes['data-ad-client']) {
-			attributes['data-ad-client'] = 'ca-pub-' + currentSlot.pub;
-		}
-		const ins = document.createElement('ins');
-		Object.keys(attributes).forEach(key => {
-			ins.setAttribute(key, attributes[key]);
-		});
-		if (!ins.classList.contains('adsbygoogle')) {
-			ins.classList.add('adsbygoogle');
-		}
-		if (!ins.classList.contains('bannerAds')) {
-			ins.classList.add('bannerAds');
-		}
-		if (islocalhost()) {
-			ins.setAttribute('data-adtest', 'on');
-		}
-		return ins;
-	}
-
-	/**
-	 * insert next other
-	 * @param {HTMLElement} newNode
-	 * @param {HTMLElement|undefined} referenceNode insert after this element
-	 */
-	function insertAfter(newNode, referenceNode) {
-		if (referenceNode) {
-			referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-		}
-	}
-
-	/**
-	 * Create detailed cookie
-	 * @param {string} name
-	 * @param {string} value
-	 * @param {number} expires
-	 * @param {string} path
-	 * @param {string} domain
-	 * @param {boolean} secure
-	 */
-	function setCookie(name, value, expires, path, domain, secure) {
-		let exp = '';
-		if (expires) {
-			const d = new Date();
-			d.setTime(d.getTime() + parseInt(expires) * 24 * 60 * 60 * 1000); // days
-			exp = '; expires=' + d.toGMTString(); // toGMTString | toUTCString
-		}
-		if (!path) {
-			path = '/';
-		}
-		const cookie =
-			name +
-			'=' +
-			encodeURIComponent(value) +
-			exp +
-			'; path=' +
-			path +
-			(domain ? '; domain=' + domain : '') +
-			(secure ? '; secure' : '');
-		log(cookie);
-		document.cookie = cookie;
-	}
-
-	/**
-	 * get all ads places
-	 * @param {Element|Document} from
-	 */
-	function getAllPlaces(from) {
-		return Array.from(from.querySelectorAll('h1,h2,h3,h4,h5,pre,header,hr,br,table,blockquote'))
-			.sort(function () {
-				return 0.5 - Math.random();
-			})
-			.filter(el => el !== null);
-	}
-
-	/**
-	 * get cookie by name
-	 * @param {string} cname
-	 * @returns
-	 */
-	function getCookie(cname) {
-		let name = cname + '=';
-		let decodedCookie = decodeURIComponent(document.cookie);
-		let ca = decodedCookie.split(';');
-		for (let i = 0; i < ca.length; i++) {
-			let c = ca[i];
-			while (c.charAt(0) == ' ') {
-				c = c.substring(1);
-			}
-			if (c.indexOf(name) == 0) {
-				return c.substring(name.length, c.length);
-			}
-		}
-		return '';
-	}
-
-	/**
-	 * Replace elements with new
-	 * @param {HTMLElement} newElement
-	 * @param {HTMLElement} oldElement
-	 */
-	function replaceWith(newElement, oldElement) {
-		if (!oldElement.parentNode) {
-			log(oldElement, 'parent null');
-			let d = document.createElement('div');
-			d.appendChild(oldElement);
-		} else {
-			//log(oldElement.parentNode.tagName);
-			oldElement.parentNode.replaceChild(newElement, oldElement);
-		}
-	}
-
-	/** FUNC END */
-}
-
-/**
- * check current script running on localhost
- * @returns
- */
-function islocalhost() {
-	// local hostname
-	if (['adsense.webmanajemen.com', 'localhost', '127.0.0.1'].includes(location.hostname)) return true;
-	// local network
-	if (location.hostname.startsWith('192.168.')) return true;
-	// port defined
-	if (location.port.length > 0) return true;
-	return false;
-}
-
+    function getParameterByName(name, url = window.location.href) {
+      name = name.replace(/[\[\]]/g, '\\$&');
+      var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    }
+  
 
 

3afc9547249dc45417bfb41b0cce1d4b28c9b054
dimaslanjaka <dimaslanjaka@gmail.com>
2023-05-22 18:28:30 +0700

    chore: released from https://github.com/dimaslanjaka/static-blog-generator-hexo/commit/06138488cab79937521abb6c191033e151791bca
    commit hash: 061384
    
    commit url: https://github.com/dimaslanjaka/static-blog-generator-hexo/commit/06138488cab79937521abb6c191033e151791bca
    
    runner: https://github.com/dimaslanjaka/static-blog-generator-hexo/commit/06138488cab79937521abb6c191033e151791bca/checks/5044992059

--- hexo-seo-js/concat-6efa191b0fa68bfa6b65970fc0dd909b.js
+++ hexo-seo-js/concat-6efa191b0fa68bfa6b65970fc0dd909b.js
@@ -1,9 +1,11 @@
 /*--- inner-0 --*/
 
 
-setTimeout(function () {
-//document.location.href = ''+newUrl+'';
-location.replace(''+newUrl+'');
-}, 3000);
+
+      setTimeout(function () {
+        //document.location.href = 'https://www.webmanajemen.com/2021/12/blogger-conditional-tags.html';
+        location.replace('https://www.webmanajemen.com/2021/12/blogger-conditional-tags.html');
+      }, 3000);
+    
 
 

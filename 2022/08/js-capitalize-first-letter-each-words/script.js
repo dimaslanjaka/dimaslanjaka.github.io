c112a7e771a59c16f79f1272d2be7578a7fec88b
dimaslanjaka <dimaslanjaka@gmail.com>
2023-07-17 19:12:52 +0700

    chore: released from https://github.com/dimaslanjaka/source-posts/commit/fcae215ae2bf33f61976736d2ac7bb1d7a1bcc5f
    commit hash: fcae21
    
    commit url: https://github.com/dimaslanjaka/source-posts/commit/fcae215ae2bf33f61976736d2ac7bb1d7a1bcc5f
    
    runner: https://github.com/dimaslanjaka/source-posts/commit/fcae215ae2bf33f61976736d2ac7bb1d7a1bcc5f/checks/5575018791

--- 2022/08/js-capitalize-first-letter-each-words/script.js
+++ 2022/08/js-capitalize-first-letter-each-words/script.js
@@ -7,14 +7,14 @@
 function capitalizer(str, moreSymbols) {
   let symbols = ["-", " "];
   if (Array.isArray(moreSymbols)) {
-    // concatenate more symbols and unique
-    symbols = [...new Set(symbols.concat(moreSymbols))];
+	// concatenate more symbols and unique
+	symbols = [...new Set(symbols.concat(moreSymbols))];
   }
   symbols.forEach((symbol) => {
-    str = str
-      .split(symbol)
-      .map((str) => str.charAt(0).toUpperCase() + str.slice(1))
-      .join(symbol);
+	str = str
+	  .split(symbol)
+	  .map((str) => str.charAt(0).toUpperCase() + str.slice(1))
+	  .join(symbol);
   });
   return str;
 }
@@ -22,12 +22,12 @@ function capitalizer(str, moreSymbols) {
 if (location.host.match(/cdpn|codepen/)) console.clear();
 Array.from(document.querySelectorAll("[data-str]")).forEach((el) => {
   const arg = el.getAttribute("data-arguments")
-    ? el.getAttribute("data-arguments").split(",")
-    : [];
+	? el.getAttribute("data-arguments").split(",")
+	: [];
   console.log(arg);
   const str = el.getAttribute("data-str");
   el.innerHTML = `<span style='color:red'>${str}</span> => <span style='color:green'>${capitalizer(
-    str,
-    arg
+	str,
+	arg
   )}</span>`;
 });

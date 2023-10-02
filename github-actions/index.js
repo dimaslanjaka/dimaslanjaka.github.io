00f47ddd5342fe8482d93bcc3d69ad26ee16c038
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-02 11:02:47 +0700

    update github validator
    Signed-off-by: dimaslanjaka <dimaslanjaka@gmail.com>
    
    Former-commit-id: b052351614cc541f1386296779129da25e30ef53

--- github-actions/index.js
+++ github-actions/index.js
@@ -5,6 +5,8 @@ const root = process.cwd();
 const jsdom = require("jsdom");
 const path = require("path");
 const yaml = require("yaml");
+const color = require("ansi-colors");
+
 /**
  * @type {import('./tmp/schema.json')}
  */
@@ -28,16 +30,22 @@ let hasErrors = false;
 (async function () {
 	Object.keys(config["validate"] || {}).forEach((name) => {
 		const full = path.resolve(root, config["validate"][name]);
-		console.log("validating", name, full.replace(root, ""));
+		console.log("validating", color.magenta(name), full.replace(root, ""));
 		validate(full, name);
 	});
+
+	if (hasErrors) process.exit(1);
 
 	const array = config["install"] || [];
 	for (let i = 0; i < array.length; i++) {
 		const p = array[i];
 		try {
 			const cwd = path.resolve(root, p);
-			await spawn("npm", ["install", "--omit=dev", "--production"], {
+			const isYarn = path.resolve(cwd, "yarn.lock");
+			// removing yarn.lock
+			if (isYarn) fs.rmSync(isYarn);
+			console.log("installing node libs", p, "->", cwd);
+			await spawn("npm", ["install", "--omit=dev" /*, "--production"*/], {
 				cwd,
 			});
 		} catch (_err) {
@@ -52,17 +60,28 @@ let hasErrors = false;
  * @param {string} as what is this file used for. ex: homepage
  */
 function validate(file, as) {
-	if (fs.statSync(file).size === 0) {
-		throw new Error(`file is empty ${as || file}`);
+	if (!fs.existsSync(file)) {
+		console.log(as, file, "is not exist");
+		hasErrors = true;
 	}
 
-	const dom = new jsdom.JSDOM(fs.readFileSync(file));
-	const { window } = dom;
-	const { document } = window;
-	const bodyEmpty = document.body.innerHTML.trim().length === 0;
-	// throw when body is empty
-	if (bodyEmpty) throw new Error(`body is empty file ${as || file}`);
+	// check size
+	if (fs.statSync(file).size === 0) {
+		console.log(`file is empty ${as || file}`);
+		hasErrors = true;
+	}
 
-	document.close();
-	window.close();
+	if (fs.existsSync(file)) {
+		const dom = new jsdom.JSDOM(fs.readFileSync(file));
+		const { window } = dom;
+		const { document } = window;
+		const bodyEmpty = document.body.innerHTML.trim().length === 0;
+		// throw when body is empty
+		if (bodyEmpty) throw new Error(`body is empty file ${as || file}`);
+
+		document.close();
+		window.close();
+	} else {
+		hasErrors = true;
+	}
 }

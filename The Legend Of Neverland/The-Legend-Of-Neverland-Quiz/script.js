00745429f6e921e0f8219a07f5c0a29b4a0cf4e3
dimaslanjaka <dimaslanjaka@gmail.com>
2023-10-09 13:17:35 +0700

    chore: released from https://github.com/dimaslanjaka/static-blog-generator-hexo/commit/3d2bf4c2b57b36e5bee2be48980f5c169bee9972
    commit hash: 3d2bf4
    
    commit url: https://github.com/dimaslanjaka/static-blog-generator-hexo/commit/3d2bf4c2b57b36e5bee2be48980f5c169bee9972
    
    runner: https://github.com/dimaslanjaka/static-blog-generator-hexo/commit/3d2bf4c2b57b36e5bee2be48980f5c169bee9972/checks/6452746567

--- The Legend Of Neverland/The-Legend-Of-Neverland-Quiz/script.js
+++ The Legend Of Neverland/The-Legend-Of-Neverland-Quiz/script.js
@@ -8,29 +8,29 @@ if (location.host == 'cdpn.io') {
   console.clear();
 
   function rangeAlphabetic(start, stop) {
-    var result = [];
-    for (
-      var idx = start.charCodeAt(0), end = stop.charCodeAt(0);
-      idx <= end;
-      ++idx
-    ) {
-      result.push(String.fromCharCode(idx));
-    }
-    return result;
+	var result = [];
+	for (
+	  var idx = start.charCodeAt(0), end = stop.charCodeAt(0);
+	  idx <= end;
+	  ++idx
+	) {
+	  result.push(String.fromCharCode(idx));
+	}
+	return result;
   }
 
   let aZ = rangeAlphabetic('a', 'z')
-    .concat(rangeAlphabetic('A', 'Z'))
-    .filter(function (el) {
-      return el != null;
-    }); // a-zA-Z array
+	.concat(rangeAlphabetic('A', 'Z'))
+	.filter(function (el) {
+	  return el != null;
+	}); // a-zA-Z array
 
   // automated test
   setTimeout(function () {
-    let inputSearch = document.getElementById('search-questions');
-    var keyword = aZ[Math.floor(Math.random() * aZ.length)];
-    inputSearch.value = keyword;
-    inputSearch.dispatchEvent(new Event('keyup'));
+	let inputSearch = document.getElementById('search-questions');
+	var keyword = aZ[Math.floor(Math.random() * aZ.length)];
+	inputSearch.value = keyword;
+	inputSearch.dispatchEvent(new Event('keyup'));
   }, 3000);
 }
 
@@ -48,11 +48,11 @@ function loadJScript(src, callback) {
   s.type = 'text/javascript';
   s.src = src;
   s.onload = s.onreadystatechange = function () {
-    //console.log( this.readyState ); //uncomment this line to see which ready states are called.
-    if (!r && (!this.readyState || this.readyState == 'complete')) {
-      r = true;
-      if (typeof callback == 'function') callback();
-    }
+	//console.log( this.readyState ); //uncomment this line to see which ready states are called.
+	if (!r && (!this.readyState || this.readyState == 'complete')) {
+	  r = true;
+	  if (typeof callback == 'function') callback();
+	}
   };
   t = document.getElementsByTagName('script')[0];
   t.parentNode.insertBefore(s, t);
@@ -66,7 +66,7 @@ function loadJScript(src, callback) {
 function uniqArr(a) {
   var seen = {};
   return a.filter(function (item) {
-    return seen.hasOwnProperty(item) ? false : (seen[item] = true);
+	return seen.hasOwnProperty(item) ? false : (seen[item] = true);
   });
 }
 
@@ -81,9 +81,9 @@ function escapeRegExp(string) {
 
 let quizUrls = [
   location.protocol +
-    '//' +
-    location.host.trim() +
-    '/The Legend Of Neverland/Quiz/quiz.txt',
+	'//' +
+	location.host.trim() +
+	'/The Legend Of Neverland/Quiz/quiz.txt',
   //'https://crossorigin.me/http://backend.webmanajemen.com/tlon/quiz.php?show',
   'https://backend.webmanajemen.com/tlon/quiz.php?show'
 ];
@@ -97,120 +97,120 @@ function jQueryMethod() {
 
   // searcher
   let searchLi = function (filter) {
-    if (!filter) {
-      console.log('input empty');
-      return;
-    }
-    let listQuiz = jQuery("ul[id*='questions'] li");
-    listQuiz.each(function (_index) {
-      // search from first characters
-      let searchFirst =
-        jQuery(this)
-          .text()
-          .search(new RegExp('^' + escapeRegExp(filter), 'gmi')) < 0;
-      if (searchFirst) {
-        jQuery(this).hide();
-      } else {
-        jQuery(this).show();
-        // move to first position
-        jQuery(this).prependTo(jQuery("ul[id*='questions']"));
-      }
+	if (!filter) {
+	  console.log('input empty');
+	  return;
+	}
+	let listQuiz = jQuery("ul[id*='questions'] li");
+	listQuiz.each(function (_index) {
+	  // search from first characters
+	  let searchFirst =
+		jQuery(this)
+		  .text()
+		  .search(new RegExp('^' + escapeRegExp(filter), 'gmi')) < 0;
+	  if (searchFirst) {
+		jQuery(this).hide();
+	  } else {
+		jQuery(this).show();
+		// move to first position
+		jQuery(this).prependTo(jQuery("ul[id*='questions']"));
+	  }
 
-      // search wildcards
-      let searchWild =
-        jQuery(this)
-          .text()
-          .search(new RegExp(escapeRegExp(filter), 'gmi')) < 0;
-      if (searchWild) {
-        jQuery(this).hide();
-      } else {
-        jQuery(this).show();
-      }
-    });
+	  // search wildcards
+	  let searchWild =
+		jQuery(this)
+		  .text()
+		  .search(new RegExp(escapeRegExp(filter), 'gmi')) < 0;
+	  if (searchWild) {
+		jQuery(this).hide();
+	  } else {
+		jQuery(this).show();
+	  }
+	});
   };
 
   // transform array to li
   let transformArray2Li = function () {
-    // clean orphan text
-    $('#questions').text('');
-    // remove existing li's
-    $('#questions li').remove();
+	// clean orphan text
+	$('#questions').text('');
+	// remove existing li's
+	$('#questions li').remove();
 
-    for (let i = 0; i < quizSrc.length; i++) {
-      let str = quizSrc[i];
-      let isTrue = /\(O\)$/i;
-      let li = document.createElement('li');
-      li.innerHTML = str;
-      if (isTrue.test(str)) {
-        li.setAttribute('class', 'isTrue');
-      } else {
-        li.setAttribute('class', 'isFalse');
-      }
-      document.getElementById('questions').appendChild(li);
-    }
+	for (let i = 0; i < quizSrc.length; i++) {
+	  let str = quizSrc[i];
+	  let isTrue = /\(O\)$/i;
+	  let li = document.createElement('li');
+	  li.innerHTML = str;
+	  if (isTrue.test(str)) {
+		li.setAttribute('class', 'isTrue');
+	  } else {
+		li.setAttribute('class', 'isFalse');
+	  }
+	  document.getElementById('questions').appendChild(li);
+	}
   };
 
   // step 1: get new question sources
   quizUrls.forEach(function (quizUrl) {
-    let url_parse = new URL(quizUrl);
-    // url_parse.search = '?uid=' + new Date();
-    // console.log('parse_query_url', parse_query_url(url_parse.toString()));
-    // console.log(url_parse.toString());
+	let url_parse = new URL(quizUrl);
+	// url_parse.search = '?uid=' + new Date();
+	// console.log('parse_query_url', parse_query_url(url_parse.toString()));
+	// console.log(url_parse.toString());
 
-    //console.log(quizUrl);
-    fetch(url_parse.toString())
-      .then(function (response) {
-        // The API call was successful!
-        return response.text();
-      })
-      .then(processResponse)
-      .catch(function () {
-        const log = 'cannot fetch ' + url_parse.toString();
-        const debugEl = document.getElementById('quiz-debug');
-        if (debugEl) {
-          debugEl.innerHTML += log + '<hr/>';
-        } else {
-          console.log(log);
-        }
-      });
+	//console.log(quizUrl);
+	fetch(url_parse.toString())
+	  .then(function (response) {
+		// The API call was successful!
+		return response.text();
+	  })
+	  .then(processResponse)
+	  .catch(function () {
+		const log = 'cannot fetch ' + url_parse.toString();
+		const debugEl = document.getElementById('quiz-debug');
+		if (debugEl) {
+		  debugEl.innerHTML += log + '<hr/>';
+		} else {
+		  console.log(log);
+		}
+	  });
   });
 
   function processResponse(data) {
-    if (typeof data === 'string') {
-      // split newLine from retrieved text into array
-      let split = data.split('\n');
-      // trim
-      quizSrc = quizSrc.map(function (str) {
-        return str.trim();
-      });
-      // merge and remove duplicates
-      quizSrc = uniqArr(
-        // merge
-        quizSrc
-          .concat(split)
-          // trim
-          .map(function (str) {
-            return str.trim();
-          })
-      )
-        // remove empties
-        .filter((str) => str.trim().length > 0);
-      // transform
-      transformArray2Li();
-    }
+	if (typeof data === 'string') {
+	  // split newLine from retrieved text into array
+	  let split = data.split('\n');
+	  // trim
+	  quizSrc = quizSrc.map(function (str) {
+		return str.trim();
+	  });
+	  // merge and remove duplicates
+	  quizSrc = uniqArr(
+		// merge
+		quizSrc
+		  .concat(split)
+		  // trim
+		  .map(function (str) {
+			return str.trim();
+		  })
+	  )
+		// remove empties
+		.filter((str) => str.trim().length > 0);
+	  // transform
+	  transformArray2Li();
+	}
   }
 
   /**
    * start searching
    */
   function doSearch() {
-    if (
-      inputSearch &&
-      inputSearch.value &&
-      inputSearch.value.trim().length > 0
-    ) {
-      searchLi(inputSearch.value);
-    }
+	if (
+	  inputSearch &&
+	  inputSearch.value &&
+	  inputSearch.value.trim().length > 0
+	) {
+	  searchLi(inputSearch.value);
+	}
   }
 
   // attach event listener
@@ -218,14 +218,14 @@ function jQueryMethod() {
   // filter only (O)
   // listen input#O_only
   $('#O_only').on('change', function (e) {
-    e.preventDefault();
-    if (this.checked) {
-      $('.isFalse').remove();
-    } else {
-      transformArray2Li();
-    }
+	e.preventDefault();
+	if (this.checked) {
+	  $('.isFalse').remove();
+	} else {
+	  transformArray2Li();
+	}
 
-    doSearch();
+	doSearch();
   });
 
   const jqInput = jQuery(inputSearch);
@@ -233,35 +233,35 @@ function jQueryMethod() {
   let listenerTimer;
   // on input typed and changed https://stackoverflow.com/a/7757327/6404439
   jqInput.on('keyup change', () => {
-    if (listenerTimer) {
-      clearTimeout(listenerTimer);
-      listenerTimer = undefined;
-    }
-    listenerTimer = setTimeout(inputListener, 700);
+	if (listenerTimer) {
+	  clearTimeout(listenerTimer);
+	  listenerTimer = undefined;
+	}
+	listenerTimer = setTimeout(inputListener, 700);
   });
 
   // form add quiz
   /*
   $("form#addQuiz").on("submit", function (e) {
-    e.preventDefault();
-    let t = $(this);
-    $.ajax({
-      url: t.attr("action"),
-      type: "post",
-      //dataType: "json",
-      data: t.serialize(),
-      success: function (data) {
-        console.log(data);
-      }
-    });
+	e.preventDefault();
+	let t = $(this);
+	$.ajax({
+	  url: t.attr("action"),
+	  type: "post",
+	  //dataType: "json",
+	  data: t.serialize(),
+	  success: function (data) {
+		console.log(data);
+	  }
+	});
   });
   */
 }
 
 if (typeof window.jQuery === 'undefined') {
   loadJScript(
-    'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js',
-    jQueryMethod
+	'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js',
+	jQueryMethod
   );
 } else {
   jQueryMethod();
@@ -280,26 +280,26 @@ function parse_query_url(url) {
   if (typeof url !== 'string') return; //throw new Error('Please provide url');
   // http://jsfiddle.net/drzaus/8EE8k/
   const deparam = (function (d, x, params, p, i, j) {
-    return function (qs) {
-      // start bucket; can't cheat by setting it in scope declaration or it overwrites
-      params = {};
-      // remove preceding non-querystring, correct spaces, and split
-      qs = qs
-        .substring(qs.indexOf('?') + 1)
-        .replace(x, ' ')
-        .split('&');
-      // march and parse
-      for (i = qs.length; i > 0; ) {
-        p = qs[--i];
-        // allow equals in value
-        j = p.indexOf('=');
-        // what if no val?
-        if (j === -1) params[d(p)] = undefined;
-        else params[d(p.substring(0, j))] = d(p.substring(j + 1));
-      }
+	return function (qs) {
+	  // start bucket; can't cheat by setting it in scope declaration or it overwrites
+	  params = {};
+	  // remove preceding non-querystring, correct spaces, and split
+	  qs = qs
+		.substring(qs.indexOf('?') + 1)
+		.replace(x, ' ')
+		.split('&');
+	  // march and parse
+	  for (i = qs.length; i > 0; ) {
+		p = qs[--i];
+		// allow equals in value
+		j = p.indexOf('=');
+		// what if no val?
+		if (j === -1) params[d(p)] = undefined;
+		else params[d(p.substring(0, j))] = d(p.substring(j + 1));
+	  }
 
-      return params;
-    }; //--  fn  deparam
+	  return params;
+	}; //--  fn  deparam
   })(decodeURIComponent, /\+/g);
   return deparam(url);
 }
@@ -314,13 +314,13 @@ function parse_url(url) {
 
 if (typeof jQuery !== 'undefined') {
   $(document).on('click', '#clear-cache', function () {
-    $.ajax({
-      url: '',
-      context: document.body,
-      success: function (s, _x) {
-        $('html[manifest=saveappoffline.appcache]').attr('content', '');
-        $(this).html(s);
-      }
-    });
+	$.ajax({
+	  url: '',
+	  context: document.body,
+	  success: function (s, _x) {
+		$('html[manifest=saveappoffline.appcache]').attr('content', '');
+		$(this).html(s);
+	  }
+	});
   });
 }
